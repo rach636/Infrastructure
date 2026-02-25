8de27edb4eb6159439a7cd5bb85c948040a89649
rach636 <59640829+rach636@users.noreply.github.com>
2026-02-25 23:35:15 +0530

    Update backend.tf

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "hospital-management-hackathon"
+    bucket  = "hospital-management-tfstate"
     key     = "prod/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
