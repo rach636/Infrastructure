3235e41d250b43b1bf0688458af3dee3f576958b
rach636 <59640829+rach636@users.noreply.github.com>
2026-02-26 02:14:31 +0530

    Update main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -12,18 +12,50 @@ locals {
       valueFrom = value_from
     }
   ]
-  effective_execution_role_arn = var.execution_role_arn != null && var.execution_role_arn != "" ? var.execution_role_arn : aws_iam_role.ecs_task_execution[0].arn
-  effective_task_role_arn      = var.task_role_arn != null && var.task_role_arn != "" ? var.task_role_arn : aws_iam_role.ecs_task[0].arn
+  execution_role_arn = aws_iam_role.ecs_task_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+}
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.app_name}-${var.environment}-${var.service_name}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
+resource "aws_iam_policy" "ecs_task_full_access_policy" {
+  name        = "${var.app_name}-${var.environment}-${var.service_name}-ecs-task-full-access"
+  description = "Single ECS role with full AWS access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "*",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_full_access_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecs_task_full_access_policy.arn
+}
 resource "aws_ecs_task_definition" "service" {
   family                   = var.service_name
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.task_cpu
   memory                   = var.task_memory
-  execution_role_arn       = local.effective_execution_role_arn
-  task_role_arn            = local.effective_task_role_arn
+  execution_role_arn = local.execution_role_arn
+task_role_arn      = local.task_role_arn
 
   container_definitions = jsonencode([{
     name      = var.service_name
@@ -94,7 +126,7 @@ resource "aws_cloudwatch_log_group" "service" {
 }
 
 # IAM Roles
-resource "aws_iam_role" "ecs_task_execution" {
+/*resource "aws_iam_role" "ecs_task_execution" {
   count = var.execution_role_arn != null && var.execution_role_arn != "" ? 0 : 1
   name  = "${var.service_name}-execution-role"
 
@@ -131,7 +163,7 @@ resource "aws_iam_role" "ecs_task" {
     }]
   })
 }
-
+*/
 data "aws_ecs_cluster" "main" {
   cluster_name = "${var.app_name}-${var.environment}-cluster"
 }
